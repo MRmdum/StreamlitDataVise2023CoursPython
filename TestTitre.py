dafaa5e9b1b36b5074c45f164a4517926e5a67c4
MRmdum <107920254+MRmdum@users.noreply.github.com>
2023-05-15 12:59:39 +0200

    Update TestTitre.py

--- TestTitre.py
+++ TestTitre.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import json
-import pycountry_convert as pc
 # datetime oprations
 from datetime import timedelta
 # to get web contents
